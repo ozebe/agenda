bc3a8abc915149201ec9d18545adce12805bef0a
Wesley Ozebe dos Santos <wesleysantos345@hotmail.com>
2019-12-20 17:21:29 -0200

    Corrigido o renderer, pegando a linha do sorter e corrigido as cores.

--- src/model/ColorirTabela.java
+++ src/model/ColorirTabela.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,68 +27,60 @@ public class ColorirTabela extends DefaultTableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         try {
             super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-            //203,216,227
-            //Color c = new Color(203,216,227);
-            int linha = tableCompromissos.getRowSorter().convertRowIndexToModel(row);
-            
-            //System.out.println("id -> "+compromissos.get(linha).getId() + " hora -> " + compromissos.get(linha).getHora());
-            String horaBase = compromissos.get(row).getHora();
-            String dataBase = compromissos.get(row).getData();
-            String dataHoraBaseString = dataBase + " " + horaBase;
-            
-//            System.out.println("dataBase "+dataBase );
-//            System.out.println("horaBase "+horaBase );
-
-            
-            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date dataHoraBase = formato.parse(dataHoraBaseString);
-            //System.out.println(""+dataHoraB);
-            
-            Color c = Color.WHITE;
-            //Object hora = table.getValueAt(row, 0); //coluna 0 no model pega a data
-             Object hora = compromissos.get(row).getHora();
-                //dd/MM/yyyy HH:mm:ss
-                
-//            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-//            Date horaNaBase = formatter.parse(hora.toString());
-
-            String horaA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-            
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date dataHoraAtual = formatter.parse(horaA);
-            
+            //formatadores
             SimpleDateFormat formataHora = new SimpleDateFormat("HH:mm:ss");
             SimpleDateFormat formataData = new SimpleDateFormat("yyyy-MM-dd");
             
-            System.out.println("Data e hora atual -> " + dataHoraAtual + " - " +  "Data e hora na base -> " + dataHoraBase);
-            //System.out.println("atual -> "+horaAtual);
-//            long hA = horaAtual.getTime();
-//            long hB = horaNaBase.getTime();
-//            long diferencaHoras = hA - hB;
+            //pega a linha correta, utilizando o rowSorter
+            int linha = tableCompromissos.getRowSorter().convertRowIndexToModel(row);
             
-//            System.out.println(""+String.format( "%03d:%02d", diferencaHoras / 3600000, ( diferencaHoras / 60000 ) % 60 ));
-//            if(){
-//                System.out.println("atual -> "+( hA / 60000 ) % 60 );
-//                System.out.println("base -> "+( hB / 60000 ) % 60 );
-//                c = Color.RED;
-//            } 
-//            if (hora != null && "12:00:00".equals(hora.toString())) {
-//                // RGB
-//                c = Color.RED;
-//                //c = new Color(126, 195, 255);
-//            }
+            Date dateHoraBancoFormatada = formataHora.parse(compromissos.get(linha).getHora());
+            String horaBancoFormatada = new SimpleDateFormat("HH:mm:ss").format(dateHoraBancoFormatada);
+
+            Date dateDataBancoFormatada = formataData.parse(compromissos.get(linha).getData());
+            String dataBancoFormatada = new SimpleDateFormat("yyyy-MM-dd").format(dateDataBancoFormatada);
+
+            String horaAtualFormatada = new SimpleDateFormat("HH:mm:ss").format(new Date());
+            Date dateHoraAtualFormatada = formataHora.parse(horaAtualFormatada);
+
+            String dataAtualFormatada = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            Date dateDataAtualFormatada = formataData.parse(dataAtualFormatada);
+
+            int d = dateDataAtualFormatada.compareTo(dateDataBancoFormatada);
+            int h = dateHoraAtualFormatada.compareTo(dateHoraBancoFormatada);
+            //dataAtual.comparteTo(dataBanco)
+            //dataAtual igual a dataBanco = 0
+            //dataAtual antes de dataBanco = <0
+            //dataAtual depois de dataBanco = >0
+            Color c = Color.WHITE;
+            if (d < 0) {
+                //verde
+                c = new Color(121, 210, 121);
+            } else if (d > 0) {
+                //vermelho
+                c = new Color(255, 77, 77);
+            } else if (d == 0) {
+                if (h < 0) {
+                    //verde
+                    c = new Color(121, 210, 121);
+                } else if (h > 0) {
+                    System.out.println(""+ horaAtualFormatada + " > " + horaBancoFormatada);
+                    //vermelho
+                    c = new Color(255, 77, 77);
+                } else if (h == 0) {
+                    c = Color.yellow;
+                }
+            }
+
             setBackground(c);
 
             if (isSelected) {
-                //setForeground(Color.BLUE);
                 setBackground(table.getSelectionBackground());
             }
-
             return this;
         } catch (ParseException ex) {
             Logger.getLogger(ColorirTabela.class.getName()).log(Level.SEVERE, null, ex);
         }
         return this;
     }
-
 }
